3d3ee3a7e0f3c102d20be17f9d87e18e2fe961c5
Brennan Colberg <brennan.colberg@gmail.com>
2018-10-19 14:22:59 -0700

    Initial Commit

---
+++ BinaryQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinaryQuiz
+//
+//  Created by Brennan Colberg on 10/19/18.
+//  Copyright © 2018 Brennan Colberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
